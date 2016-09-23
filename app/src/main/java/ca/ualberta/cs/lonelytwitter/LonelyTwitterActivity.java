35a59a0e0cb042d2aa220f8e56e0c3d29ac0bf77
Stephen Larsen <stephen@uws111628usr.uws.ualberta.ca>
2016-09-22 18:35:56 -0600

    Lab3 exercise. Implemented Gson, implemented clearButton.

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -0,0 +1,120 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Date;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class LonelyTwitterActivity extends Activity {
+
+	private static final String FILENAME = "file.sav";
+	private EditText bodyText;
+	private ListView oldTweetsList;
+	private ArrayList<Tweet> tweetList = new ArrayList<Tweet>();
+	private ArrayAdapter<Tweet> adapter;
+
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		bodyText = (EditText) findViewById(R.id.body);
+		Button saveButton = (Button) findViewById(R.id.save);
+		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
+
+		Button clearButton = (Button) findViewById(R.id.clear);   //
+
+		saveButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				setResult(RESULT_OK);
+				String text = bodyText.getText().toString();
+				Tweet newTweet = new NormalTweet(text);
+				tweetList.add(newTweet);
+				adapter.notifyDataSetChanged();
+				saveInFile();
+
+			}
+		});
+
+		clearButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				setResult(RESULT_OK);
+				tweetList.clear();
+				adapter.notifyDataSetChanged();
+				saveInFile();
+			}
+		});
+
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		adapter = new ArrayAdapter<Tweet>(this,
+				R.layout.list_item, tweetList);
+		oldTweetsList.setAdapter(adapter);
+	}
+
+	private void loadFromFile() {
+		ArrayList<String> tweets = new ArrayList<String>();
+		try {
+			FileInputStream fis = openFileInput(FILENAME);
+			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+			Gson gson = new Gson();
+
+			// Code taken from http://stackoverflow.com/questions/12384064/gson-convert-from-json-to-a-typed-arraylistt
+			// on September 22, 2016
+			Type listType = new TypeToken<ArrayList<NormalTweet>>(){}.getType();
+			tweetList = gson.fromJson(in, listType);
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException();
+		}
+	}
+	
+	private void saveInFile() {
+		try {
+			FileOutputStream fos = openFileOutput(FILENAME, 0);
+
+			OutputStreamWriter writer = new OutputStreamWriter(fos);
+			Gson gson = new Gson();
+			gson.toJson(tweetList, writer);
+			writer.flush();
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException();
+		}
+	}
+}
